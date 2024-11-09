c1884b0468e5d5ade67cba089f989faf40292866
Stéphane FEREY <stephane.ferey@gmail.com>
2024-11-09 15:53:22 +0100

    feat: Ajout DevIcons feat: Ajout ToolboxSection

---
+++ src/configs/dev-icons.ts
@@ -0,0 +1,21 @@
+import { iconPaths } from '@components/IconPaths';
+
+export const devIcons = [
+	{ icon: iconPaths.html5, name: 'HTML' },
+	{ icon: iconPaths.css3, name: 'CSS' },
+	{ icon: iconPaths.javascript, name: 'JavaScript' },
+	{ icon: iconPaths.php, name: 'PHP' },
+	{ icon: iconPaths.git, name: 'Git' },
+	{ icon: iconPaths.zend, name: 'Zend' },
+	{ icon: iconPaths.symfony, name: 'Symfony' },
+	{ icon: iconPaths.postgresql, name: 'PostgreSQL' },
+	{ icon: iconPaths.docker, name: 'Docker' },
+	{ icon: iconPaths.nodejs, name: 'Node.js' },
+	{ icon: iconPaths.flutter, name: 'Flutter' },
+	{ icon: iconPaths.typescript, name: 'TypeScript' },
+	{ icon: iconPaths.graphql, name: 'GraphQL' },
+	{ icon: iconPaths.tailwindcss, name: 'Tailwind CSS' },
+	{ icon: iconPaths.go, name: 'Go' },
+	{ icon: iconPaths.googlecloud, name: 'Google Cloud' },
+	{ icon: iconPaths.astro, name: 'Astro' },
+];
