08805751bd4c40bc44f1b9fb9b1194a59c11a93b
Stéphane FEREY <stephane.ferey@gmail.com>
2024-11-09 13:44:31 +0100

    feat: Ajout du .devcontainer

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,4 +2,10 @@
 import { defineConfig } from 'astro/config';
 
 // https://astro.build/config
-export default defineConfig({});
+export default defineConfig({
+    site: 'https://sferey.com',
+    server: {
+        port: 4321,
+        host: true,
+    },
+});
